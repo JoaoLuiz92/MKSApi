eaf00ac052d4e88307ae701ff56d58a12294cb7e
JoaoLuiz92 <jluizdarosajr@gmail.com>
2024-05-16 23:06:09 -0300

    ajustes

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,6 +8,6 @@ import { User } from '../entities/user.entity';
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService],
-  exports: [UsersService], // Certifique-se de exportar UsersService
+  exports: [UsersService], 
 })
 export class UsersModule {}
